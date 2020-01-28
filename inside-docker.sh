10daedf93dfc9622ccb4f47ac1a95fd10a67fc54
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2020-01-28 21:17:08 +0100

    added wheels

---
+++ inside-docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# To build wheels for this project, call `build-wheels` after
+# having customized this script
+#
+# NB: this script is supposed to be run inside one of the manylinux
+# docker images. It is a companion to the build-wheels
+
+set -e -x
+
+PROJ=$1
+
+# Put here any build instructions for c extensions
+# $ git clone ...
+# $ cd ...
+# $ ./configure && make && make install
+yum install -y fftw-devel
+yum install -y libsndfile-devel
+
+# Compile wheels. Customize the wildcard to match the desired python versions
+for PYBIN in /opt/python/cp3[7-8]*/bin; do
+    "${PYBIN}/pip" install --upgrade pip
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+# for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/$PROJ*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done 
+
