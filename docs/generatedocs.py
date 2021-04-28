3d36b50b8a28bbb719739dab79aaebab94c24f0e
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2021-04-28 09:30:32 +0200

    MANIFEST.in

--- docs/generatedocs.py
+++ docs/generatedocs.py
@@ -7,9 +7,9 @@ try:
     from loristrck import util
     from emlib import doctools
 except ImportError:
-    print("Trying to update documentation, but the python present in the current environment"
-          "does not have the needed packages (loristrck, emlib). Documentation will not be"
-          "updated")
+    print("\nWARNING: Trying to update documentation, but the python present in the current environment"
+          " does not have the needed packages (loristrck, emlib). Documentation will not be"
+          " updated\n")
     sys.exit(0)
 
 
