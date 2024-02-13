8992d52c31b7c2bc273be4ef74ea135fd6979a8d
gesellkammer <eduardo.moguillansky@gmail.com>
2024-02-13 15:26:16 +0100

    upload artifacts

--- test/test-stretch.py
+++ test/test-stretch.py
@@ -1,9 +1,13 @@
 import loristrck as lt
 import sndfileio
 import argparse
+import os
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outfile', default='test1.wav')
 args = parser.parse_args()
+
+outfolder = 'testout'
 
 sndfile = "sound/musicbox-tchaikovsky-44k1-1.flac"
 minpartialdur = 0.008
@@ -12,12 +16,16 @@ samples, sr = sndfileio.sndread(sndfile)
 samples0 = sndfileio.util.getchannel(samples, 0)
 partials = lt.analyze(samples0, sr, resolution=80)
 selection = lt.util.partials_between(partials, 0, 2)
+
 # Remove very short partials
 selection = [p for p in selection if p[-1, 0] - p[0, 0] > minpartialdur]
 selection = [lt.util.partial_crop(p, 0, 2) for p in selection]
 stretched = lt.util.partials_stretch(selection, 20)
-print(f"Synthesizing stretched version: '{args.outfile}'")
+
 synthesized = lt.synthesize(stretched, sr)
-sndfileio.sndwrite(args.outfile, synthesized, sr=sr)
+
+outfile = os.path.join(outfolder, args.outfile)
+print(f"Synthesizing stretched version: '{outfile}'")
+sndfileio.sndwrite(outfile, synthesized, sr=sr)
 print(f"Found {len(partials)} partials, reduced to {len(selection)}")
 
